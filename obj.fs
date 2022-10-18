3636dc40e0ef356de45c4b4a3ca15dd699971f31
Louis Forestier <louis.forestier@etu.univ-poitiers.fr>
2022-10-19 00:18:25 +0200

    fresnel réparé et joli

--- obj.fs
+++ obj.fs
@@ -9,7 +9,12 @@ uniform samplerCube uSampler;
 
 vec3 adaptDir(vec3 dir)
 {
-	return vec3(dir.x,dir.z,dir.y);
+	return dir.xzy;
+}
+
+vec3 adaptDir(vec4 dir)
+{
+	return dir.xzy;
 }
 
 float fresnelFactor(vec3 i, vec3 m, float ni)
@@ -17,7 +22,7 @@ float fresnelFactor(vec3 i, vec3 m, float ni)
 	float c = max(0.0,dot(i,m));
 	float g = sqrt(ni*ni+c*c-1.0);
 	float f = 1.0/2.0 * pow(g-c,2.0)/pow(g+c,2.0) * ( 1.0 + (pow(c * (g+c) - 1.0,2.0)/pow(c * (g-c) - 1.0,2.0)));
-	//float f = 1.0/2.0 * ((g - c)*(g-c)/(g+c)*(g+c)) * ( 1.0 + ((c * (g+c) - 1.0)*(c * (g+c) - 1.0))/((c * (g-c) - 1.0)*(c * (g-c) - 1.0)) );
+	//bad job, does not seem to work -> float f = 1.0/2.0 * ((g - c)*(g-c)/(g+c)*(g+c)) * ( 1.0 + ((c * (g+c) - 1.0)*(c * (g+c) - 1.0))/((c * (g-c) - 1.0)*(c * (g-c) - 1.0)) );
 	return f;
 }
 
@@ -25,10 +30,10 @@ float fresnelFactor(vec3 i, vec3 m, float ni)
 vec4 refractSkybox(vec3 pos, vec3 normal, mat4 invRotMatrix, float ind1, float ind2)
 {
 	vec3 Vo = normalize(pos);
-	vec4 Vi = vec4(refract(Vo,normal,ind1/ind2),1.0);
-	Vi = invRotMatrix * Vi;
+	vec4 Vt = vec4(refract(Vo,normal,ind1/ind2),1.0);
+	Vt = invRotMatrix * Vt;
 
-	return vec4(textureCube(uSampler,vec3(Vi.x,Vi.z,Vi.y)).rgb,1.0);
+	return vec4(textureCube(uSampler,adaptDir(Vt)).rgb,1.0);
 }
 
 vec4 reflectSkybox(vec3 pos, vec3 normal, mat4 invRotMatrix)
@@ -37,7 +42,7 @@ vec4 reflectSkybox(vec3 pos, vec3 normal, mat4 invRotMatrix)
 	vec4 Vi = vec4(reflect(Vo,normal),1.0);
 	Vi = invRotMatrix * Vi;
 
-	return vec4(textureCube(uSampler,vec3(Vi.x,Vi.z,Vi.y)).rgb,1.0);
+	return vec4(textureCube(uSampler,adaptDir(Vi)).rgb,1.0);
 }
 
 
@@ -47,16 +52,16 @@ vec4 fresnelEffect(vec3 pos, vec3 normal, mat4 invRotMatrix, float ind1, float i
 	vec3 Vo = normalize(pos);
 
 	vec4 Vi = vec4(reflect(Vo,normal),1.0);
+	float f = fresnelFactor(Vi.xyz,normal,ind2);
 	Vi = invRotMatrix * Vi;
 
-	vec4 mColor = vec4(textureCube(uSampler,vec3(Vi.x,Vi.z,Vi.y)).rgb,1.0);
+	vec4 mColor = vec4(textureCube(uSampler,adaptDir(Vi)).rgb,1.0);
 
 	vec4 Vt = vec4(refract(Vo,normal,ind1/ind2),1.0);
 	Vt = invRotMatrix * Vt;
 
-	vec4 tColor = vec4(textureCube(uSampler,vec3(Vt.x,Vt.z,Vt.y)).rgb,1.0);
+	vec4 tColor = vec4(textureCube(uSampler,adaptDir(Vt)).rgb,1.0);
 
-	float f = fresnelFactor(Vi.xyz,normal,ind2);
 	return f * mColor + (1.0-f) * tColor;
 }
 
@@ -70,5 +75,5 @@ void main(void)
 	//gl_FragColor = refractSkybox(pos3D.xyz, normalize(N),invRotMatrix,1.0,1.52);
 	
 	//gl_FragColor = reflectSkybox(pos3D.xyz, normalize(N),invRotMatrix);
-	gl_FragColor = fresnelEffect(pos3D.xyz, normalize(N),invRotMatrix,1.0,1.0);
+	gl_FragColor = fresnelEffect(pos3D.xyz, normalize(N),invRotMatrix,1.0,1.52);
 }
